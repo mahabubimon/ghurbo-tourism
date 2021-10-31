4b1c2f4f09a3dec3575e4e5606c5c97fa7386bba
mahabubimon <mahabubemon24@gmail.com>
2021-10-31 20:24:45 +0600

    test

--- src/components/pages/dashboard/manageOrders/ManageOrders.js
+++ src/components/pages/dashboard/manageOrders/ManageOrders.js
@@ -1,22 +1,41 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Dropdown, Table } from "react-bootstrap";
+import { Button, Dropdown, Table } from "react-bootstrap";
 import { ImBin } from "react-icons/im";
 
 const ManageOrders = () => {
   const [allOrders, setAllOrders] = useState([]);
+
   useEffect(() => {
     axios
       .get("https://ghurbo-tourism.herokuapp.com/orders")
       .then((res) => {
-        console.log(res.data);
         setAllOrders(res.data);
-        if (res.data.insertedId) {
-          alert("added successfully");
-        }
       })
       .catch((err) => console.log(err));
   }, []);
+
+  const deleteOrder = (id) => {
+    const proceed = window.confirm("Are you want to delete?");
+    if (proceed) {
+      const url = `https://ghurbo-tourism.herokuapp.com/orders/${id}`;
+      axios
+        .delete(url)
+        .then((res) => {
+          console.log(res);
+          // if (res.data.deletedCount > 0) {
+          const remainingAllOrders = allOrders.filter(
+            (order) => order._id !== id
+          );
+          console.log(res.data);
+          setAllOrders(remainingAllOrders);
+          // }
+        })
+        .catch((error) => {
+          console.error("There was an error!", error);
+        });
+    }
+  };
 
   return (
     <div>
@@ -32,30 +51,32 @@ const ManageOrders = () => {
           </tr>
         </thead>
         <tbody>
-            {allOrders.map((order) => (
-              <tr key={order._id}>
-                <td>{order.name}</td>
-                <td>{order.email}</td>
-                <td>{order.package}</td>
-                <td>{order.price}</td>
-                <td>
-                  <Dropdown>
-                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                      Pending
-                    </Dropdown.Toggle>
+          {allOrders.map((order) => (
+            <tr key={order._id}>
+              <td>{order.name}</td>
+              <td>{order.email}</td>
+              <td>{order.package}</td>
+              <td>{order.price}</td>
+              <td>
+                <Dropdown>
+                  <Dropdown.Toggle variant="success" id="dropdown-basic">
+                    Pending
+                  </Dropdown.Toggle>
 
-                    <Dropdown.Menu>
-                      <Dropdown.Item href="#/action-1">Pending</Dropdown.Item>
-                      <Dropdown.Item href="#/action-2">Approve</Dropdown.Item>
-                      <Dropdown.Item href="#/action-3">Delivered</Dropdown.Item>
-                    </Dropdown.Menu>
-                  </Dropdown>
-                </td>
-                <td>
-                  <ImBin></ImBin>
-                </td>
-              </tr>
-            ))}
+                  <Dropdown.Menu>
+                    <Dropdown.Item href="#/action-1">Pending</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">Approve</Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">Delivered</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </td>
+              <td>
+                <Button onClick={() => deleteOrder(order._id)}>
+                  <ImBin />
+                </Button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     </div>
